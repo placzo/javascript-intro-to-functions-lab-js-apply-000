9e59a99f0771e4b0f1ebcf1029bdcd26c17c66cd
Peter Laczo <peter.laczo@blackrock.com>
2019-03-12 11:52:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,23 +2,28 @@ function shout(string) {
   return string.toUpperCase()
 }
 
+
 function whisper(string) {
   return string.toLowerCase()
 }
 
-var grama = "hello"
 
+var grama = "hello"
 function sayHiToGrandma(grama) {
   return ("I can't hear you!")
 }
 
+
 var grama2 = "HELLO"
 function sayHiToGrandma(grama2) {
   return ("YES INDEED!")
+}
+
 
 function logShout(string) {
   console.log(string.toUpperCase())
 }
+
 
 function logWhisper(string) {
   console.log(string.toLowerCase())
